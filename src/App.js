09a89bc704f5e2717c529c95521f2cb657c0b322
Akash Gandhi <akashgandhi@mail.usf.edu>
2019-07-06 01:35:01 -0400

    Testing new deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/header/Navbar"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Projects from "./components/projects/index"
+
+function Root() {
+  return <h2>Root</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Resume() {
+  return <h2>Resume</h2>;
+}
+
+
+function Certification(){
+  return <h2>Certification</h2>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div>
+       <Router>
+    
+      <Navbar/>
+
+      <Route exact path="/" component={Root} />
+      <Route path="/about" component={About} />
+      <Route path="/resume" component={Resume} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/certification" component={Certification} />
+      </Router>
+      
     </div>
+  
   );
 }
 
