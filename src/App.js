8064a8510257039373570c08fd38a3e26ab8ecfb
Akash Gandhi <akashgandhi@mail.usf.edu>
2019-07-28 19:51:32 -0400

    Compartmentalising jsx and js

--- src/App.js
+++ src/App.js
@@ -1,129 +1,124 @@
-import React,{Component} from 'react';
-import './App.css';
-import Navbar from "./components/header/Navbar"
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/header/Navbar";
 import { HashRouter as Router, Route } from "react-router-dom";
-import Projects from "./components/projects/index"
-import Resume from "./components/resume/resume"
-import googlelogin from "./components/social/index"
-import Home from "./components/home/index"
-import About from "./components/about/index"
+import Projects from "./components/projects/index";
+import Resume from "./components/resume/";
+import googlelogin from "./components/social/index";
+import Home from "./components/home/index";
+import About from "./components/about/index";
 
-import {connect} from "react-redux"
-import {fetchLogin} from "./actions/actions"
-import {TextField,Button} from "@material-ui/core"
-import api from "./api/client"
-
+import { connect } from "react-redux";
+import { fetchLogin } from "./actions/actions";
+import { TextField, Button } from "@material-ui/core";
+import api from "./api/client";
 
 class App extends Component {
-
-  constructor(props){
-super(props)
-this.state={output:"not connected to backend"}
-this.Root=this.Root.bind(this)
-this.About=this.About.bind(this)
-this.Certification=this.Certification.bind(this)
-this.MyApp=this.MyApp.bind(this)
-this.handleFormSubmit=this.handleFormSubmit.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = { output: "not connected to backend" };
+    this.Root = this.Root.bind(this);
+    this.About = this.About.bind(this);
+    this.Certification = this.Certification.bind(this);
+    this.MyApp = this.MyApp.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
-  
 
+  Root() {
+    return <Home />;
+  }
 
-   Root() {
-    return <Home/>
+  About() {
+    return <About />;
   }
-  
-   About() {
-    return <About/>
-  }
-  
+
   // function Resume() {
   //   return <h2>Resume</h2>;
   // }
-  
-  
-   Certification(){
-    return <h2>Certification</h2>
+
+  Certification() {
+    return <h2>Certification</h2>;
   }
 
-  async handleFormSubmit(event){
-    event.preventDefault()
-    
-    let formdata=document.getElementById("submission")
-    const form=new FormData(formdata)
-    let json=null
-    form.forEach((value,key)=>{
-      json={data:value}
-    })
+  async handleFormSubmit(event) {
+    event.preventDefault();
 
-    const p=await api.post("/send",json)
-    
-    if(p.status===200){
-      
-      this.setState({output:p.data})
+    let formdata = document.getElementById("submission");
+    const form = new FormData(formdata);
+    let json = null;
+    form.forEach((value, key) => {
+      json = { data: value };
+    });
+
+    const p = await api.post("/send", json);
+
+    if (p.status === 200) {
+      this.setState({ output: p.data });
     }
-
-
 
     // //GET request
     // const p=await api.get()
     // console.log(p.data)
     // if(p.status===200){
-      
+
     //   this.setState({output:p.data})
     // }
   }
-  
-   MyApp() {
-  return this.props.auth?
-  <div style={{marginLeft:"10%"}}>
-  <h1>DemoApp - Backend Implementation in progress</h1>
-  <h3>Coming Soon!</h3>
-  <form name="submission" id="submission" onSubmit={this.handleFormSubmit}>
- <TextField
- name="searchbox"
- value={this.state.value}
- ></TextField>
- <Button name="submit" type="submit" color="primary" variant="contained" style={{marginLeft:"5%"}}>
-   submit
- </Button>
- </form>
- <pre>
-   {JSON.stringify(this.state.output,2,null)}
- </pre>
-  </div>
-  :<h2>Login failed</h2>
- 
-  }
 
+  MyApp() {
+    return this.props.auth ? (
+      <div style={{ marginLeft: "10%" }}>
+        <h1>DemoApp - Backend Implementation in progress</h1>
+        <h3>Coming Soon!</h3>
+        <form
+          name="submission"
+          id="submission"
+          onSubmit={this.handleFormSubmit}
+        >
+          <TextField name="searchbox" value={this.state.value}></TextField>
+          <Button
+            name="submit"
+            type="submit"
+            color="primary"
+            variant="contained"
+            style={{ marginLeft: "5%" }}
+          >
+            submit
+          </Button>
+        </form>
+        <pre>{JSON.stringify(this.state.output, 2, null)}</pre>
+      </div>
+    ) : (
+      <h2>Login failed</h2>
+    );
+  }
 
   render() {
     return (
-   
       <div>
-         <Router 
-         //history={history}
-         >
-      
-        <Navbar/>
-  
-        <Route exact path="/" component={this.Root} />
-        <Route path="/about" component={this.About} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/certification" component={this.Certification} />
-        <Route path="/others" component={googlelogin} />
-        <Route path="/app" component={this.MyApp} />
+        <Router
+        //history={history}
+        >
+          <Navbar />
+
+          <Route exact path="/" component={this.Root} />
+          <Route path="/about" component={this.About} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/certification" component={this.Certification} />
+          <Route path="/others" component={googlelogin} />
+          <Route path="/app" component={this.MyApp} />
         </Router>
-        
       </div>
-  
-    
-    )
+    );
   }
 }
 
-const mapStateToProps=state=>({
-  auth:state.auth.signSuccess
-})
+const mapStateToProps = state => ({
+  auth: state.auth.signSuccess
+});
 
-export default connect (mapStateToProps,{fetchLogin})(App);
+export default connect(
+  mapStateToProps,
+  { fetchLogin }
+)(App);
---
+++ src/components/resume/index.js
@@ -0,0 +1,103 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+//import Toolbar from '@material-ui/core/Toolbar';
+//import Button from '@material-ui/core/Button';
+//import { mergeClasses } from '@material-ui/styles';
+import Divider from "@material-ui/core/Divider";
+
+const useStyles = makeStyles(theme => ({
+  heading: {
+    marginTop: "2%",
+    marginLeft: "2%",
+    marginBottom: "2%"
+  },
+  body: {
+    marginTop: 8,
+    marginLeft: "2%",
+    marginBottom: "1%"
+  }
+}));
+
+function Resume() {
+  const classes = useStyles();
+  const objective = (
+    <React.Fragment>
+      <Typography
+        component="h4"
+        variant="h4"
+        className={classes.heading}
+        color="primary"
+      >
+        Objective
+      </Typography>
+      <Typography component="body" variant="body" className={classes.body}>
+        body
+      </Typography>
+    </React.Fragment>
+  );
+
+  const skills = (
+    <React.Fragment>
+      <Typography
+        component="h4"
+        variant="h4"
+        className={classes.heading}
+        color="primary"
+      >
+        Skills
+      </Typography>
+      <Typography component="body" variant="body" className={classes.body}>
+        body
+      </Typography>
+    </React.Fragment>
+  );
+
+  const experience = (
+    <React.Fragment>
+      <Typography
+        component="h4"
+        variant="h4"
+        className={classes.heading}
+        color="primary"
+      >
+        Work Experience
+      </Typography>
+      <Typography component="body" variant="body" className={classes.body}>
+        body
+      </Typography>
+    </React.Fragment>
+  );
+
+  const education = (
+    <React.Fragment>
+      <Typography
+        component="h4"
+        variant="h4"
+        className={classes.heading}
+        color="primary"
+      >
+        Education
+      </Typography>
+      <Typography component="body" variant="body" className={classes.body}>
+        body
+      </Typography>
+    </React.Fragment>
+  );
+  return (
+    <div>
+      {objective}
+      <Divider />
+      {skills}
+
+      <Divider />
+      {experience}
+      <Divider />
+      {education}
+      <Divider />
+      <Divider />
+    </div>
+  );
+}
+
+export default Resume;
