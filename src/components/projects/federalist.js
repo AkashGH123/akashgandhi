4409c21ebaf5c88bba1c91abe649c04ff0299fb6
Akash Gandhi <akashgandhi@mail.usf.edu>
2019-07-28 18:18:26 -0400

    Added Code Formatter

--- src/components/projects/federalist.js
+++ src/components/projects/federalist.js
@@ -1,61 +1,51 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import FederalistImage from "../static/images/FederalistImage.jpg"
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import FederalistImage from "../static/images/FederalistImage.jpg";
 
 const useStyles = makeStyles(theme => ({
-    card: {
-      maxWidth: 345,
-      marginLeft:"5%",
-      marginTop:"2%"
-    },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    button:{
-        marginBottom:1,
-        marginTop:10,
-        MarginLeft:10,
-        align:"right"
-    }
-  }));
-  
+  card: {
+    maxWidth: 345,
+    marginLeft: "5%",
+    marginTop: "2%"
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  button: {
+    marginBottom: 1,
+    marginTop: 10,
+    MarginLeft: 10,
+    align: "right"
+  }
+}));
 
 function Federalist(props) {
-    const classes = useStyles();
-    const {title,subheader,image,description}=props
+  const classes = useStyles();
+  const { title, subheader, image, description } = props;
 
-    return (
-        <div>
-        <Card className={classes.card}>
-      <CardHeader
-        title={title}
-        subheader={subheader}
-      />
-      <CardMedia
-        className={classes.media}
-        image={image}
-        title={title}
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary">
-      {description}
-        </Typography>
-      </CardContent>
-           <Button color="primary">
-               Open
-           </Button>
-    </Card>
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardHeader title={title} subheader={subheader} />
+        <CardMedia className={classes.media} image={image} title={title} />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary">
+            {description}
+          </Typography>
+        </CardContent>
+        <Button color="primary">Open</Button>
+      </Card>
     </div>
-    )
+  );
 }
 
-export default Federalist
+export default Federalist;
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
-import Federalist from "./federalist"
+import React, { Component } from "react";
+import Federalist from "./federalist";
 
 class Projects extends Component {
-    render() {
-        return (
-            <div>
-<Federalist title="The Federalist papers" subheader="Skills : Data Analysis with Python" description="Brief description" image={process.env.PUBLIC_URL+"/images/FederalistImage.jpg"} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Federalist
+          title="The Federalist papers"
+          subheader="Skills : Data Analysis with Python"
+          description="Brief description"
+          image={process.env.PUBLIC_URL + "/images/FederalistImage.jpg"}
+        />
+      </div>
+    );
+  }
 }
 
-export default Projects
+export default Projects;
