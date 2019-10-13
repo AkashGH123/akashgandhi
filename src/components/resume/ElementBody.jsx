a3d52a17b6b106caaa380e36fd94ec47550864c6
Akash Gandhi <akashgandhi@mail.usf.edu>
2019-10-13 17:23:01 -0400

    removing console message

--- src/components/resume/ElementBody.jsx
+++ src/components/resume/ElementBody.jsx
@@ -3,7 +3,7 @@ import { Typography } from "@material-ui/core";
 
 function ElementBody(props) {
   const { classes, body } = props;
-  console.log(body.split("*"));
+
   return body.split("*").map(item => {
     return (
       <Typography
